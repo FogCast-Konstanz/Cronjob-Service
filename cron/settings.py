357fcf7b80b8104d4b2eefa2eb72e17e82c342ef
Lukas Benner <lukasbenner@mailbox.org>
2024-11-02 23:04:48 +0100

    fix settings.py to load settings correctly

--- cron/settings.py
+++ cron/settings.py
@@ -1,12 +1,8 @@
-#!/usr/bin/python3
-
 import pathlib
-
-
 import json
 import os
 
-class Settings: 
+class Settings:
     def __init__(self):
         self.data_dir = None
         self.model_ids_path = None
@@ -16,27 +12,28 @@ class Settings:
         self.log_dir = None
 
 def load_settings():
-    package_dir = pathlib.Path(__package__).parent.resolve()
+    file_dir = pathlib.Path(__file__).parent
+    project_root = file_dir.parent.resolve()
     # Load default settings from settings.json
-    settings_path = package_dir / "settings.json"
+    settings_path = project_root / "settings.json"
     with open(settings_path, "r") as f:
         settings_json = json.load(f)
 
     # Check if settings.user.json exists and load it if it does
-    user_settings_path = package_dir / "settings.user.json"
+    user_settings_path = project_root / "settings.user.json"
     if os.path.exists(user_settings_path):
         with open(user_settings_path, "r") as f:
             user_settings = json.load(f)
-        
+
         # Merge user settings, overriding defaults where necessary
         settings_json.update(user_settings)
-    
+
      # Convert paths to be relative to the root of the repository
     settings = Settings()
     for key, value in settings_json.items():
         # Convert paths to be relative to the package directory if they end in '_path' or '_dir'
         if isinstance(value, str) and (key.endswith("_path") or key.endswith("_dir")):
-            value = os.path.join(package_dir, value) if not os.path.isabs(value) else value
+            value = os.path.join(project_root, value) if not os.path.isabs(value) else value
         # Set the attribute on the Settings instance if it exists
         if hasattr(settings, key):
             setattr(settings, key, value)
