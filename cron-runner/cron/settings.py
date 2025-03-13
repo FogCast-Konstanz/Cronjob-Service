b65944c67cc9d302678a6e6732ecf7712337b599
Lukas Benner <lukasbenner@mailbox.org>
2025-03-13 20:49:26 +0100

    fix reading of settings

--- cron-runner/cron/settings.py
+++ cron-runner/cron/settings.py
@@ -13,6 +13,20 @@ class Settings:
     def __repr__(self):
         return f"<Settings {self.__dict__}>"
 
+def merge_dict(default, override):
+    for key, value in override.items():
+        if key in default and isinstance(default[key], dict) and isinstance(value, dict):
+            merge_dict(default[key], value)
+        else:
+            default[key] = value
+
+def convert_paths(obj, project_root):
+    for key, value in obj.items():
+        if isinstance(value, str) and (key.endswith("_path") or key.endswith("_dir")):
+            obj[key] = os.path.join(project_root, value) if not os.path.isabs(value) else value
+        elif isinstance(value, dict):
+            convert_paths(value, project_root)
+
 def load_settings():
     file_dir = pathlib.Path(__file__).parent
     project_root = file_dir.parent.resolve()
@@ -28,18 +42,11 @@ def load_settings():
         with open(user_settings_path, "r") as f:
             user_settings = json.load(f)
 
-        # Merge user settings, overriding defaults where necessary
-        settings_json.update(user_settings)
+        # Merge user settings, overriding defaults where necessary using recursive merge
+        merge_dict(settings_json, user_settings)
 
     # Convert paths to be relative to the root of the repository
-    def convert_paths(obj):
-        for key, value in obj.items():
-            if isinstance(value, str) and (key.endswith("_path") or key.endswith("_dir")):
-                obj[key] = os.path.join(project_root, value) if not os.path.isabs(value) else value
-            elif isinstance(value, dict):
-                convert_paths(value)
-    
-    convert_paths(settings_json)
+    convert_paths(settings_json, project_root)
 
     # Return a Settings instance initialized with the JSON data
     return Settings(**settings_json)
