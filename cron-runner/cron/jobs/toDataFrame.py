5f3fe685bd16e77f9840d0e6c7bf887c614a6d63
Lukas Benner <lukasbenner@mailbox.org>
2025-04-08 12:56:00 +0200

    Make cutting the data easier.
    Cut when "temperature_2m", "relative_humidity_2m", "dew_point_2m" are NaN

--- cron-runner/cron/jobs/toDataFrame.py
+++ cron-runner/cron/jobs/toDataFrame.py
@@ -23,28 +23,6 @@ def toDataFrame(response, hourly_fields: list[str]):
 
 def extract_model_data(response, hourly_fields: list[str]):
     df = toDataFrame(response, hourly_fields)
-
-    # Determine forecast horizon by detecting a significant increase in missing forecast data.
-    # Compute the number of NaNs in each row for forecast columns.
-    nan_counts = df[hourly_fields].isnull().sum(axis=1)
-
-    horizon_index = None
-    diff_threshold = 10  # threshold for a significant jump in missing values
-    for i in range(1, len(nan_counts)):
-        diff = nan_counts.iloc[i] - nan_counts.iloc[i-1]
-        if diff >= diff_threshold:
-            horizon_index = df.index[i]
-            break
-
-    if horizon_index is not None:
-        df = df.loc[:horizon_index-1]
-
-    # Additionally, drop any row that is entirely NaN in the forecast columns.
-    # Remove only trailing rows where all forecast columns are NaN
-    for idx in df.index[::-1]:
-        if df.loc[idx, hourly_fields].isnull().all():
-            df = df.drop(idx)
-        else:
-            break
-    
+    # Cut the dataframe when the most important values are not forecasted anymore (NaN)
+    df = df.dropna(subset=["temperature_2m", "relative_humidity_2m", "dew_point_2m"])
     return df
